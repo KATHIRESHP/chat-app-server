e8537c656af5e34a547ffba2ef7d5eaef7cbb18d
KATHIRESHP <kathireshpalanisamy055@gmail.com>
2023-07-25 12:08:49 +0530

    1208070223

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ io.on("connection", (socket) => {
     global.chatSocket = socket;
     socket.on("add-user", (userId) => {
         onlineUsers.set(userId, socket.id);
-    })
+    });
 
     socket.on("send-msg", (data) => {
         const sendUserSocket = onlineUsers.get(data.to);
