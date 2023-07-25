2f173c50615878a71e883599ee1752c86397d4cf
KATHIRESHP <kathireshpalanisamy055@gmail.com>
2023-07-25 11:50:51 +0530

    115025072023

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const socket = require('socket.io');
 require('dotenv').config();
 
 const app = express()
-app.use(express.json({limit: "2mb"}))
+app.use(express.json({limit: "3mb"}))
 app.use(cors())
 
 app.use("/api/auth", userRoutes);
@@ -30,7 +30,7 @@ const server =  app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors:{
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5000",
         credentials: true
     }
 })
